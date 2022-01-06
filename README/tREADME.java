39dbc685808fb11d7f3559c4bd40e49549bb6c37
Shahbaj <shahbajs987@gmail.com>
2022-01-05 23:16:09 -0500

    Update tREADME.java

--- README/tREADME.java
+++ README/tREADME.java
@@ -1,20 +1,11 @@
 
-public class tREADME {
+public class tREADME 
+{   // SUPERIOR NEWLINE BRACKET
 
 
     //      SHAHBAJ SINGH
     //
-    //      GITHUB: https://github.com/shahbajsingh 
-    //      LINKEDIN: https://www.linkedin.com/in/shahbaj-singh-84b12a202/
-    //      FACEBOOK: https://www.facebook.com/shahbaj.singh.1694/ 
-    //      TWITTER: https://twitter.com/i2147483647d 
     //      EMAIL: shahbajs987@gmail.com
-
-    public static void main(String [] args){
-
-        System.out.println("test");
-
-    }
-
+    //
     
 }
